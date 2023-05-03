818f9991852d0a97440488687a647ea05280e2cf
mineclover <mineclover@naver.com>
2023-05-04 02:07:31 +0900

    env : prettierrc setting

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,17 @@
-import { createBrowserRouter, Route, createRoutesFromElements, Navigate } from 'react-router-dom'
-import Tutorial from './router/tutorial'
-import LoginTest from './router/loginTest'
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  Navigate,
+} from 'react-router-dom';
+import Tutorial from './router/tutorial';
+import LoginTest from './router/loginTest';
 
 const router = createBrowserRouter(
-  createRoutesFromElements([<Route path="/01" element={<Tutorial />} />, <Route path="/02" element={<LoginTest />} />])
-)
+  createRoutesFromElements([
+    <Route path="/01" element={<Tutorial />} />,
+    <Route path="/02" element={<LoginTest />} />,
+  ])
+);
 
-export { router }
+export { router };
