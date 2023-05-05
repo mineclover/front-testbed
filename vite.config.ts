6770f8cc80752b951ed866e64ef764448f257a78
mineclover <mineclover@naver.com>
2023-05-06 03:46:27 +0900

    bug : vite failed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import typescript from 'rollup-plugin-typescript2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  esbuild: false,
+  plugins: [react(), typescript()],
+});
